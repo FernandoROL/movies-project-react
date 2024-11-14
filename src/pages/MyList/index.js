2be7ceaede08879276d0673b1b6ac280a4f0373b
FernandoROL <felhopez@gmail.com>
2024-11-14 15:21:31 -0300

    saved list

--- src/pages/MyList/index.js
+++ src/pages/MyList/index.js
@@ -1,11 +1,57 @@
+import { Link } from 'react-router-dom';
 import './style.css';
+import { useEffect, useState } from 'react';
 
 function MyList(){
+
+    const [movies, setMovies] = useState([]);
+
+    useEffect(() => {
+        
+        const myList = localStorage.getItem('@fernandoflix');
+        setMovies(JSON.parse(myList) || []);
+
+    }, []);
+
+    function removeMovie(movieId){
+        let moviefilter = movies.filter((item) => {
+            return (item.id !== movieId);
+        });
+        const removeLocal = () => {
+            setMovies(moviefilter);
+            localStorage.setItem("@fernandoflix", JSON.stringify(moviefilter));
+        }
+        window.confirm("Do you really want to delete this movie from your list?", )? 
+        removeLocal()
+        :void(0);
+        return;
+    }
+    
     return(
-        <div>
+        <div className='my-movies'>
             <h1>
-                MyList
+                My List
             </h1>
+
+            {
+                movies.length == 0 ? <span>List empty...</span> : void(0)
+            }
+            
+            <ul>
+                {
+                    movies.map((movie) => {
+                        return(
+                            <li key={movie.id}>
+                                <span>{movie.title}</span>
+                                <div>
+                                    <Link to={`/movies/${movie.id}`}>Open details</Link>
+                                    <button onClick={() => removeMovie(movie.id)}>Remove</button>
+                                </div>
+                            </li>
+                        );
+                    })
+                }
+            </ul>
         </div>
     );
 }
